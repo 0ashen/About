f8686ca1fd4f4387ccb83ae4bc6c190fd81cb938
0ashen <iusfilipp@gmail.com>
2020-12-15 06:19:20 +0300

    fix's

--- pages/about-me-skills-contacts/about-me-skills-contacts.enum.ts
+++
@@ -1,4 +0,0 @@
-export enum NameSpacesEnum {
-    'common' = 'common',
-    'page-about-me-skills-contacts' = 'page-about-me-skills-contacts'
-}
--- pages/about-me-skills-contacts/index.tsx
+++ pages/about-me-skills-contacts/index.tsx
@@ -1,10 +1,19 @@
 import { Head } from '../../components/Head'
 import { GeneralLayout } from '../../components/GeneralLayout/GeneralLayout'
 import { withTranslation } from '../../i18n'
-import { AboutMeSkillsContactsProps } from './about-me-skills-contacts.interface'
-import { NameSpacesEnum } from './about-me-skills-contacts.enum'
 import { Skills } from '../../components/Skills/Skills'
+import { TFunction } from 'next-i18next'
 
+export interface AboutMeSkillsContactsProps {
+    readonly t: TFunction,
+    // i18n: I18n,
+    // namespacesRequired: NameSpacesEnum[]
+}
+
+export enum NameSpacesEnum {
+    'common' = 'common',
+    'page-about-me-skills-contacts' = 'page-about-me-skills-contacts'
+}
 
 function AboutMeSkillsContacts({ t }: AboutMeSkillsContactsProps): JSX.Element {
 
