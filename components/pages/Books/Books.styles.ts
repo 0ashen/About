4d35c48dc9dd1e6c604c7adca791aaad0b6a8ceb
0ashen <iusfilipp@gmail.com>
2020-12-24 12:19:52 +0300

    stage page projects

--- components/pages/Books/Books.styles.ts
+++ components/pages/Books/Books.styles.ts
@@ -4,4 +4,5 @@ export const Wrapper = styled.div`
     display: flex;
     flex-wrap: wrap;
     justify-content: space-around;
+    padding-top: 100px;
 `
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -2,26 +2,24 @@ import { Head } from '../components/Head'
 import { GeneralLayout } from '../components/GeneralLayout/GeneralLayout'
 import { withTranslation } from '../i18-next'
 import { PageProjectsNamespaceEnum } from '../components/pages/projects/projects-translate-name-space.enum'
-
+import { Projects } from '../components/pages/projects/Projects'
 
 function PageProjects({ t }): JSX.Element {
     return (
         <>
             <Head>
                 <title>
-                    {
-                        t(`${PageProjectsNamespaceEnum['common']}:common-browser-tab-title-prefix`) +
-                        t('browser-tab-title')
-                    }
+                    {t(
+                        `${PageProjectsNamespaceEnum['common']}:common-browser-tab-title-prefix`
+                    ) + t('browser-tab-title')}
                 </title>
             </Head>
             <GeneralLayout>
-                Projects
+                <Projects />
             </GeneralLayout>
         </>
     )
 }
-
 
 PageProjects.getInitialProps = async () => ({
     namespacesRequired: [
@@ -31,7 +29,6 @@ PageProjects.getInitialProps = async () => ({
 })
 
 export default withTranslation([
-        PageProjectsNamespaceEnum['page-projects'],
-        PageProjectsNamespaceEnum['common'],
-    ],
-)(PageProjects)
+    PageProjectsNamespaceEnum['page-projects'],
+    PageProjectsNamespaceEnum['common'],
+])(PageProjects)
